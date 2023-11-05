5822af482e9741e45c754bb9ef912ab6b07b06c0
zeeshanshaanu <zeeshanzahoor409@gmail.com>
2023-11-05 15:59:19 +0500

    fix error in NoInternet file

--- src/components/connectivity/NoInternet .tsx
+++ src/components/connectivity/NoInternet .tsx
@@ -1,25 +1,26 @@
 import React from "react";
-import { useLottie } from "lottie-react";
-import { Box } from "@mui/material";
+// import { useLottie } from "lottie-react";
+// import { Box } from "@mui/material";
 
-import animationData from "../../../public/Assets/LottieFile/No-Internet.json";
+// import animationData from "../../../public/Assets/LottieFile/No-Internet.json";
 
 const NoInternet = () => {
-  const options = {
-    animationData: animationData,
-    loop: true,
-  };
-  const { View } = useLottie(options);
+  // const options = {
+  //   animationData: animationData,
+  //   loop: true,
+  // };
+  // const { View } = useLottie(options);
 
   return (
     <div className="">
-      <div className="CenterDiv">
+      hello
+      {/* <div className="CenterDiv">
         <Box width={170} height={170}>
           <div className="text-center">{View}</div>
         </Box>
         <h2 className="mt-5 font-semibold">Looks like you're offline</h2>
         <small>please check your internet connection and try again.</small>
-      </div>
+      </div> */}
     </div>
   );
 };
